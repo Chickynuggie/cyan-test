bd0f65fd7d6750a265b4e01465ca0318ade1b218
Daniel Farkas <daniel.farkas.nsc@gmail.com>
2021-04-20 10:57:09 +0200

    accounts list styles and modifications

--- src/components/AccountList.js
+++ src/components/AccountList.js
@@ -5,14 +5,25 @@ import { selectUser } from '../actions';
 class AccountList extends React.Component {
 
     render() {
-        return <div className='account-list' onClick={() => this.props.selectUser("3")}>AccountList</div>;
-    }
+        return (<div className='account-list'>
+            {this.props.pagedProfiles.map(profile => <div
+                key={profile.id}
+                className='account-list__card'
+                onClick={() => this.props.selectUser(profile.id.toString())}>
+                    <div className='account-list__card__top'>
+                        <img className='account-list__card__top__avatar' src={`https://randomuser.me/api/portraits/women/${profile.id}.jpg`} alt='avatar' />
+                        <div className='account-list__card__top__info'>{profile.name}</div>
+                    </div>
+                    <div className='account-list__card__info'><div>Country</div><div>{profile.country || 'not provided'}</div></div>
+                    <div className='account-list__card__info'><div>City: </div><div>{profile.city || 'not provided'}</div></div>
+                    <div className='account-list__card__info'><div>Address: </div><div>{profile.address || 'not provided'}</div></div>
+            </div>)}
+        </div>)
+    };
 }
 
-const mapStateToProps = (state) => {
-    return {
-        usersByPage: state.userData,
-    };
+const mapStateToProps = () => {
+    return {};
 };
 
 export default connect(mapStateToProps, { selectUser })(AccountList);
